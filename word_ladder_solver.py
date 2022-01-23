0ac04a9bd320f73c55814303dd1bce443a53bcb4
Lennier Mulder <lenniermulder@icloud.com>
2022-01-23 11:11:58 -0800

    added word checker function, currently set up to pass randomly generated words through the checker, but thinking of simply choosing from the list

--- word_ladder_solver.py
+++ word_ladder_solver.py
@@ -1,5 +1,26 @@
+# TODO: Save all the 4letter words into a file.
+# TODO: Generate a word that is one letter different from startWord and
+# continue to endWord
 from os.path import dirname
+from string import ascii_lowercase
 import os.path
+
+
+def word_checker(word1, word2):
+    if word2 not in fourLetterWords:
+        return False
+    else:
+        if word2 != word1:  # exactly one letter different
+            diffrences = 0
+            for i in range(len(word2)):
+                if word2[i] != word1[i]:
+                    diffrences += 1
+            if diffrences != 1:
+                return False
+            else:
+                return True
+
+
 cur_dir = dirname(__file__)
 fourLetterWords = list()
 with open(os.path.join(cur_dir, "words_alpha.txt")) as wordsfile:
@@ -7,3 +28,8 @@ with open(os.path.join(cur_dir, "words_alpha.txt")) as wordsfile:
         if len(line) == 5:
             fourLetterWords += [line[:4]]
 print(fourLetterWords)
+
+
+def solve(startWord, endWord, length):
+    for c in ascii_lowercase:
+        pass
